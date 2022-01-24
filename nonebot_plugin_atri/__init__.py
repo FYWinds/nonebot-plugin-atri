79038a12ed8b9b76c6bac59bc23dd90b2ae7575a
FYWinds <i@windis.cn>
2022-01-24 14:53:21 +0800

    :tada: v2.0.0  适配B1 修改调用方式

--- nonebot_plugin_atri/__init__.py
+++ nonebot_plugin_atri/__init__.py
@@ -3,45 +3,38 @@
 import re
 import random
 from difflib import SequenceMatcher
-from nonebot.adapters.cqhttp import Bot, GroupMessageEvent, GROUP
-from nonebot.typing import T_State
-from nonebot.plugin import on_message
-from nonebot.rule import to_me
-from nonebot.log import logger
-from nonebot.adapters.cqhttp.message import MessageSegment
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, GROUP, MessageSegment
+from nonebot.plugin import on_command
 from .data import atri_text
 from .data import V_PATH
-__plugin_name__ = "高性能萝卜子"
-__usage__ = "@Bot 想说的话"
 
-atri = on_message(rule=to_me(), permission=GROUP, priority=50)
+__plugin_name__ = "ATRI 语音包"
+__usage__ = "atri"
+
+atri = on_command("atri", permission=GROUP, priority=50)
 
 
 @atri.handle()
-async def _h(bot: Bot, event: GroupMessageEvent, state: T_State):
-    args = str(event.get_message()).strip()
-    if args:
-        state["words"] = args
-
-
-@atri.got("words", prompt="想对萝卜子说什么话呢?")
-async def _g(bot: Bot, event: GroupMessageEvent, state: T_State):
-    words = state["words"]
-    diff: dict[str, float] = {}
-    for text in atri_text:
-        r1 = SequenceMatcher(None, words, text["s"]).ratio()
-        r2 = SequenceMatcher(None, words, text["s_f"]).ratio()
-        r3 = SequenceMatcher(None, words, text["s_k"]).ratio()
-        diff.update({text["o"]: r1 * r2 + r3})  # 完全瞎想的计算方式，没啥特殊的意义
-    diff_sorted = dict(
-        sorted(diff.items(), key=lambda item: item[1], reverse=True))
-    voice = random.choice(
-        [
-            list(diff_sorted.keys())[0],
-            list(diff_sorted.keys())[1],
-            list(diff_sorted.keys())[2],
-        ]
-    )
+async def _h(bot: Bot, event: GroupMessageEvent):
+    words = str(event.message).strip()
+    if words:
+        diff: dict[str, float] = {}
+        for text in atri_text:
+            r1 = SequenceMatcher(None, words, text["s"]).ratio()
+            r2 = SequenceMatcher(None, words, text["s_f"]).ratio()
+            r3 = SequenceMatcher(None, words, text["s_k"]).ratio()
+            diff.update({text["o"]: r1 * r2 + r3})  # 完全瞎想的计算方式，没啥特殊的意义
+        diff_sorted = dict(
+            sorted(diff.items(), key=lambda item: item[1], reverse=True))
+        voice = random.choice(
+            [
+                list(diff_sorted.keys())[0],
+                list(diff_sorted.keys())[1],
+                list(diff_sorted.keys())[2],
+            ]
+        )
+    else:
+        voice = random.choice(atri_text)["o"]
     text = re.findall("(.*).mp3", voice)[0]
     await atri.send(MessageSegment.record(f"file:///{V_PATH}{voice}"))
     await atri.finish(text)
--- nonebot_plugin_atri/data.py
+++ nonebot_plugin_atri/data.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 import os
 import json
+from typing import Dict, List
 from pathlib import Path
 
 current_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".") + "/"
@@ -11,7 +12,7 @@ T_PATH = str(Path(f"{current_path}resources/text").absolute()) + "/"
 
 
 with open(f"{T_PATH}atri.json", "r", encoding="utf-8") as file:
-    atri_text = json.load(file)
+    atri_text: List[Dict[str, str]] = json.load(file)
 
 
 if __name__ == "__main__":
